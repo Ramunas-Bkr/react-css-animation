6ad0d0064341871d0012fc2b2e5e75ce4f946d16
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-06-08 09:16:09 +0300

    CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition } from 'react-transition-group';
 
 function App() {
 
   const [toogle, setToogle] = useState(true)
+  const [toogle2, setToogle2] = useState(true)
 
   return (
     <div className="container">
       <button onClick={() => { setToogle(!toogle) }}>Toogle</button>
+      <button onClick={() => { setToogle2(!toogle2) }}>Toogle2</button>
       <hr />
       <div className={"blocks"}></div>
         <Transition
@@ -20,6 +22,16 @@ function App() {
           unmountOnExit>
           {state => <div className={`square blue ${state}`}>{state}</div>}
         </Transition>
+        <CSSTransition
+          in={toogle2}
+          timeout={1000}
+          classNames="os"
+          mountOnEnter
+          unmountOnExit>
+          <div className={`square orange`}>
+            {toogle2.toString()}
+          </div>
+        </CSSTransition>
     </div>
   );
 }
