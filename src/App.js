0352d82af29b9618ba398e19086cf3f77abecfca
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-06-08 06:36:17 +0300

    css animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [toogle, setToogle] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={() => {setToogle(!toogle)}}>Toogle</button>
+      <hr />
+      <div className={"blocks"}></div>
+        {toogle && <div className={"square blue"}>{toogle.toString()}</div>}
+     
     </div>
   );
 }
